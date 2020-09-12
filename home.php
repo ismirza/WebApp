58f45e10a7b9528232bfb38307ffad1d918efcea
Ibrokhim Mirzakhonov <46466176+ismirza@users.noreply.github.com>
2020-09-12 10:46:17 -0400

    Add files via upload

---
+++ home.php
@@ -0,0 +1,34 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?>
+
+ <table>
+  <tr>
+    <th>Week 1</th>
+    <th>Week 2</th>
+    <th>Week 3</th>
+    <th>Week 4</th>
+    <th>Week 5</th>
+    <th>Week 6</th>
+    <th>Week 7</th>
+  </tr>
+  <tr>
+    <td >click here</td>
+    <td>click here</td>
+    <td>click here</td>
+    <td>click here</td>
+    <td>click here</td>
+    <td>click here</td>
+    <td>click here</td>
+  </tr>
+</table>
+
+<a href="http://ismirza.it.pointpark.edu/webapp/phpinfo.php">Phpinfo</a>
+
+
+
+ </body>
+</html>
